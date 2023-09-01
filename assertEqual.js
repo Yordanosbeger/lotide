cf1e26e39ac597bea0b9d7f6373cb96529f83092
Yordanos Gebreselasie <yordibege@gmail.com>
2023-09-01 06:45:22 -0600

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); // This will fail
+assertEqual(1, 1); // This will pass
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
