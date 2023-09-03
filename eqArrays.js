422c52017c0afcf1488267e6887de5c041182333
Yordanos Gebreselasie <yordibege@gmail.com>
2023-09-03 01:40:46 -0600

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should pass
+assertEqual(eqArrays([], []), true); // should pass
+assertEqual(eqArrays([1], []), false); // should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // should pass
