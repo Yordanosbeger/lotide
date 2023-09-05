39f42c91c023003f44a307865c308b20f7703cba
Yordanos Gebreselasie <yordibege@gmail.com>
2023-09-05 07:30:31 -0600

    countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,18 @@
+// countOnly function implementation
+
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+  for (let item of allItems) {
+    
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+        
+      }
+    }
+  }
+  return results;
+};
+
